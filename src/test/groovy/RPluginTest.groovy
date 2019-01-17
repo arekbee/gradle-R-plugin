1ba120882b1c111eafae09c29e8e246d02680f26
arekbee <arekbee@gmail.com>
2019-01-17 22:20:47 +0100

    Fixing utest

--- src/test/groovy/RPluginTest.groovy
+++ src/test/groovy/RPluginTest.groovy
@@ -2,13 +2,11 @@ import org.arekbee.RCode
 import org.gradle.api.Project
 import org.gradle.internal.impldep.org.junit.Test
 
-class GreetingPluginTest {
+class RPluginTest {
     @Test
     public void greeterPluginAddsGreetingTaskToProject() {
         Project project = ProjectBuilder.builder().build()
-        project.pluginManager.apply 'org.samples.greeting'
-
-        assertTrue(project.tasks.build instanceof RCode)
+        project.pluginManager.apply 'org.arekbee.gradle-R-plugin'
         assertTrue(project.tasks.restore instanceof RCode)
     }
 }
